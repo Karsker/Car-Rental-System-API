67681ef69f69a171675453d0fd64cd5e293beab7
Karthik Chandrasekhar <karthikchandrasekhar03@outlook.com>
2024-11-24 10:55:47 +0530

    Initial Commit

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using CarRentalSystem.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CarRentalSystem.Data
+{
+    public class AppDbContext: DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
